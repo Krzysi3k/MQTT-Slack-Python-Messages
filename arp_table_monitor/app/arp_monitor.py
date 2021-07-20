2c80aeca15b9bafc6782121d35617e5e013ca8ae
Krzysi3k <global.takeover@o2.pl>
2021-07-20 02:34:17 +0200

    refactoring

--- arp_table_monitor/app/arp_monitor.py
+++ arp_table_monitor/app/arp_monitor.py
@@ -17,15 +17,16 @@ def send_slack_alert(devices: List[str]):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     data = {
         'channel': '#smart-home',
-        'text': f'{now}\nnew device(s) just connected to home network:\n{devices}'
+        'text': f'{now}\nnew device(s) just connected home network:\n{devices}'
     }
     r = requests.post(api_url, headers=headers, json=data)
 
 
 def read_arp_table() -> List[Dict[str, str]]:
-    p = subprocess.Popen("arp -a | awk '{print $1\",\"$2\",\"$4}'" , shell=True, stdout=subprocess.PIPE)
+    cmd = 'arp -a | grep -v incomplete | awk \'{print $1","$2","$4}\''
+    p = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE)
     output = p.stdout.read().decode('utf-8').split('\n')
-    lst = [i for i in output if len(i) > 0 and 'incomplete' not in i]
+    lst = [i for i in output if len(i) > 0]
     
     items = []
     for i in lst:
@@ -45,17 +46,22 @@ def read_arp_table() -> List[Dict[str, str]]:
     return items
 
 
+def detect_new_devices() -> List[str]:
+    file = Path(__file__).parent / 'registered_devices.txt'
+    new_devices = []
+    with open(file=file, mode='r+') as f:
+        known_macs = f.read().split('\n')
+        current_macs = read_arp_table()
+        for i in current_macs:
+            if not i['hwaddr'] in known_macs:
+                new_devices.append(i)
+                f.writelines(i['hwaddr'] + '\n')
+        return new_devices
+
+
 if __name__ == '__main__':
     while True:
-        file = Path(__file__).parent / 'registered_devices.txt'
-        new_devices = []
-        with open(file=file, mode='r+') as f:
-            known_macs = f.read().split('\n')
-            current_macs = read_arp_table()
-            for i in current_macs:
-                if not i['hwaddr'] in known_macs:
-                    new_devices.append(i)
-                    f.writelines(i['hwaddr'] + '\n')
-        if len(new_devices) > 0:
-            send_slack_alert(new_devices)
+        devices = detect_new_devices()
+        if len(devices) > 0:
+            send_slack_alert(devices)
         sleep(60 * 5)
